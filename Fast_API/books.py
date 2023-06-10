1bc56e6311eea04b452c4bfa2f288812fa6586f7
Mikhail Sidorenko <sido6232@protonmail.ch>
2023-06-10 17:13:25 +0200

    failed experiment with pandas

--- Fast_API/books.py
+++ Fast_API/books.py
@@ -54,3 +54,9 @@ async def read_all_books(book_title: str):
 @app.post('/books/create_book')
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
+    
+@app.put('/books/update_book')
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book

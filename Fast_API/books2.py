0f6b96166942f467b0b2e6f1faf1262b62bf5eec
Mikhail Sidorenko <sido6232@protonmail.ch>
2023-06-12 22:55:00 +0200

    modified:   Fast_API/books2.py

--- Fast_API/books2.py
+++ Fast_API/books2.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -17,6 +18,14 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id: int
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=5)
+    
+
 BOOKS = [
     Book(1, 'The idiot', 'Fyodor Dostoevsky', 'An exploration of the meaning of love and life', 5),
     Book(2, 'Notes from Underground', 'Fyodor Dostoevsky', 'A hyperconscious man reflecting on his life', 3),
@@ -32,6 +41,11 @@ async def read_all_books():
 @app.post('/create-book')
 async def create_book(book_request=Body()):
     BOOKS.append(book_request)
+    
+@app.post('/create-book2')
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.dict())
+    BOOKS.append(new_book)
 
 #wip delete function
 #@app.delete('/books/delete-book/{book_id}')

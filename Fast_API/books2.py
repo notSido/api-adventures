9635f50605dc8724d4800c7e43f9601914612878
Mikhail Sidorenko <sido6232@protonmail.ch>
2023-06-15 19:58:57 +0200

    modified:   Fast_API/books2.py

--- Fast_API/books2.py
+++ Fast_API/books2.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Body, HTTPException, Path, Query
 from pydantic import BaseModel, Field
 from typing import Optional
+from starlette import status
 
 app = FastAPI()
 
@@ -51,23 +52,23 @@ BOOKS = [
 
 
 
-@app.get('/books')
+@app.get('/books', status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
     
-@app.post('/create-book')
+@app.post('/create-book', status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict())
     BOOKS.append(find_book_id(new_book))
     
-@app.get('/books/{book_id}')
+@app.get('/books/{book_id}', status_code=status.HTTP_200_OK)
 async def fetch_book(book_id:int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
     raise HTTPException(status_code=404, detail='item not found')   
 
-@app.get('/books/')
+@app.get('/books/', status_code=status.HTTP_200_OK)
 async def read_book_by_rating(rating:int = Query(gt=0, lt=6)):
     books_to_return = []
     for book in BOOKS:
@@ -83,7 +84,7 @@ def find_book_id(book: Book):
     
     return book
 
-@app.put('/book/update_book')
+@app.put('/book/update_book', status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book: BookRequest):
     book_changed = False
     for i in range(len(BOOKS)):
@@ -93,7 +94,7 @@ async def update_book(book: BookRequest):
     if not book_changed:
         raise HTTPException(status_code=404, detail='Target not found')
 
-@app.delete('/books/{book_id}')
+@app.delete('/books/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id:int = Path(gt=0)):
     book_deleted = False
     for i in range(len(BOOKS)):
@@ -103,7 +104,7 @@ async def delete_book(book_id:int = Path(gt=0)):
     if not book_deleted:
         raise HTTPException(status_code=404, detail='Target not found')
         
-@app.get('/books/published/')
+@app.get('/books/published/', status_code=status.HTTP_200_OK)
 async def read_book_by_release(published_date:int = Query(gt=1699, lt=3001)):
     book_found = False
     books_to_return = []

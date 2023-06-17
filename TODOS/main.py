8d9a987f3633845174326cb9a95985330e8140c3
Mikhail Sidorenko <sido6232@protonmail.ch>
2023-06-18 01:31:47 +0200

    Update main.py

--- TODOS/main.py
+++ TODOS/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 import models
 from models import ToDos
 from database import engine, SessionLocal
@@ -16,6 +16,15 @@ def get_db():
     finally:
         db.close()
 
+db_dependancy = Annotated[Session, Depends(get_db)]
+
 @app.get('/')
 async def read_all(db: Annotated[Session, Depends(get_db)]):
     return db.query(ToDos).all()
+
+@app.get('/todo/{todo_id}')
+async def read_by_id(db: db_dependancy, todo_id: int):
+    todo_model = db.query(ToDos).filter(ToDos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail='not found')

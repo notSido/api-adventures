322ab0598f62ec98c58b40a2956bbb9d249e2869
Mikhail Sidorenko <sido6232@protonmail.ch>
2023-06-26 08:53:50 +0200

    modified:   TODOS/models.py 	modified:   TODOS/routers/auth.py

--- TODOS/routers/auth.py
+++ TODOS/routers/auth.py
@@ -1,8 +1,31 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
 
 router = APIRouter()
 
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated=['auto'])
 
-@router.get('/auth/')
-async def get_user():
-    return {'user': 'authenticated'}
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+@router.post('/auth/')
+async def create_user(CreateUserRequest: CreateUserRequest):
+    create_user_model = Users(
+        email = CreateUserRequest.email,
+        username = CreateUserRequest.username,
+        first_name = CreateUserRequest.first_name,
+        last_name = CreateUserRequest.last_name,
+        role = CreateUserRequest.role,
+        hashed_password = CreateUserRequest.password,
+        is_active = True
+    )
+    
+    return create_user_model

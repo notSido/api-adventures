8f2e0fb068828b59e2cb357f180847d8dabe4846
Mikhail Sidorenko <sido6232@protonmail.ch>
2023-07-23 22:23:02 +0200

    modified:   TODOS/database.py     message: transitioned to mysql, supports postgresql connection

--- TODOS/database.py
+++ TODOS/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-sqlalchemy_database_URL = 'sqlite:///./todosapp.db'
+sqlalchemy_database_URL = 'mysql+pymysql://BigBoss:letmein1@127.0.0.1:3306/todos'
+#sqlalchemy_database_URL = 'postgresql://postgres:letmein1@localhost/TodosApplicationDatbase'
 
-engine = create_engine(sqlalchemy_database_URL, connect_args={'check_same_thread': False})
+engine = create_engine(sqlalchemy_database_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

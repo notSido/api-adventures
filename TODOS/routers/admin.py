b5f331a5ae3e4fbced292f2c206156723e96957c
Mikhail Sidorenko <sido6232@protonmail.ch>
2023-07-23 18:45:39 +0200

    modified:   TODOS/routers/admin.py 	modified:   TODOS/todosapp.db

--- TODOS/routers/admin.py
+++ TODOS/routers/admin.py
@@ -27,7 +27,21 @@ user_dependancy = Annotated[dict, Depends(getcurrentuser)]
 
 @router.get('/todo', status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependancy, db: db_dependancy):
+    
     if user is None or user.get('user_role') != 'admin':
         raise HTTPException(status_code=401, detail='Authentication Failed')
 
     return db.query(ToDos).all()
+
+@router.delete('/todo/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(user: user_dependancy, db: db_dependancy, todo_id: int = Path(gt=0)):
+   
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')    
+    
+    todo_model = db.query(ToDos).filter(ToDos.id == todo_id).first()
+    
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not found')
+    db.query(ToDos).filter(ToDos.id == todo_id).delete()
+    db.commit()
